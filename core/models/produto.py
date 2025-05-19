5e3eb2d3ea9c3fc01865a0fab2a3e295f8288ea4
nicoleabrand <nicoleabrand13@gmail.com>
2025-05-19 14:23:14 -0300

    fix: sem input de imagem em produtos por enquanto

--- core/models/produto.py
+++ core/models/produto.py
@@ -9,7 +9,7 @@ class Produto(models.Model):
     preco = models.DecimalField(max_digits=10, decimal_places=2)
     categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name='produtos')
     estoque = models.IntegerField(default=0)
-    imagem = models.ImageField(upload_to='produtos/')
+    # imagem = models.ImageField(upload_to='produtos/')
     grao = models.ForeignKey(Grao, on_delete=models.PROTECT, related_name='produtos', blank=True, null=True)
 # fazer uma tabela de grãos (variações de cafe)
 
