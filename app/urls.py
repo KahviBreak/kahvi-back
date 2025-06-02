382ebf14fa7313e88f69e75ed5818d8133b3d4d3
nicoleabrand <nicoleabrand13@gmail.com>
2025-06-02 14:39:11 -0300

    feat: adicionando o uploader e cloudinary

--- app/urls.py
+++ app/urls.py
@@ -6,7 +6,9 @@ from drf_spectacular.views import (
     SpectacularSwaggerView,
 )
 from rest_framework.routers import DefaultRouter
-
+from django.conf import settings
+from django.conf.urls.static import static
+from uploader.router import router as uploader_router
 from core.views import UserViewSet, CategoriaViewSet, ProdutoViewSet, GraoViewSet, EnderecoViewSet, CompraViewSet, PedidoViewSet
 
 router = DefaultRouter()
@@ -36,4 +38,7 @@ urlpatterns = [
     ),
     # API
     path('api/', include(router.urls)),
+    path("api/media/", include(uploader_router.urls)),  
+
 ]
+urlpatterns += static(settings.MEDIA_ENDPOINT, document_root=settings.MEDIA_ROOT)
--- core/models/produto.py
+++ core/models/produto.py
@@ -1,6 +1,8 @@
 from django.db import models
 from core.models.categoria import Categoria
 from core.models.graocafe import Grao
+from uploader.models import Image
+
 
 
 class Produto(models.Model):
@@ -12,6 +14,13 @@ class Produto(models.Model):
     # imagem = models.ImageField(upload_to='produtos/')
     grao = models.ForeignKey(Grao, on_delete=models.PROTECT, related_name='produtos', blank=True, null=True)
 # fazer uma tabela de grãos (variações de cafe)
-
+    imagem = models.ForeignKey(
+        Image,
+        related_name="+",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        default=None,
+    )
     def __str__(self):
         return self.nome
