988b6bb51bfc36e528bcf44ef3ee6944ab650d50
nicoleabrand <nicoleabrand13@gmail.com>
2025-09-01 09:05:33 -0300

    passage: vou me matar

--- app/settings.py
+++ app/settings.py
@@ -150,7 +150,9 @@ REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'PAGE_SIZE': 10,
      'DEFAULT_AUTHENTICATION_CLASSES': ('core.authentication.TokenAuthentication',), # Autenticação no passage.id
-    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly', ),  # autorização de acesso
+    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',         
+    'rest_framework.permissions.IsAuthenticated',
+),  # autorização de acesso
 }
 
 PASSAGE_APP_ID = os.getenv('PASSAGE_APP_ID', 'app_id')
