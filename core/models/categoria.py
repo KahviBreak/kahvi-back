dc728be26f584e1fb8ab125acc7511467fcb4d6e
nicoleabrand <nicoleabrand13@gmail.com>
2025-09-08 09:20:02 -0300

    .

--- core/models/categoria.py
+++ core/models/categoria.py
@@ -1,7 +1,15 @@
 from django.db import models
-
+from uploader.models import Image
 class Categoria(models.Model):
     nome = models.CharField(max_length=255, unique=True)
+    imagem = models.ForeignKey(
+        Image,
+        related_name="+",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        default=None,
+    )
 
     def __str__(self):
         return self.nome
