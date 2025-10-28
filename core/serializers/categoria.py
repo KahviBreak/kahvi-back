148640832d57727ac660c6b69dc0ce47d92d488b
vallentini <vallentinigabrielli.silva@gmail.com>
2025-10-28 15:06:12 -0300

    model serializer categoria

--- core/serializers/categoria.py
+++ core/serializers/categoria.py
@@ -1,7 +1,10 @@
 from rest_framework.serializers import ModelSerializer
 from core.models import Categoria
+from uploader.serializers import ImageSerializer
+
 
 class CategoriaSerializer(ModelSerializer):
+    imagem = ImageSerializer(required=False, read_only=True)
     class Meta:
         model = Categoria
         fields = '__all__'
